b084d2400fa76a3a5944eddabd68244ee043da52
DESU SHREE UMA MAHESWARI <desushreeumamaheswari2004@gmail.com>
2026-02-05 15:02:31 +0530

    Add files via upload

---
+++ BoxingUnboxing/BoxingUnboxing/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.AccessControl;
+
+int num = 2;
+Object obj = num;
+Console.WriteLine(obj);
+int number = (int)obj;
+Console.WriteLine(number);
