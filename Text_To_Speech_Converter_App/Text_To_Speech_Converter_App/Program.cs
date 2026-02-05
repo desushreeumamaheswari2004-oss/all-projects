724a1c9cae205ebbc65bfc6855e691bdf9640f7b
DESU SHREE UMA MAHESWARI <desushreeumamaheswari2004@gmail.com>
2026-02-05 14:22:06 +0530

    Add files via upload

---
+++ Text_To_Speech_Converter_App/Text_To_Speech_Converter_App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;// For Text to Speech functionality
+
+namespace Text_To_Speech_Converter_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.Speak("Hello, welcome to the Text to Speech Converter Application developed in .NET Framework!");
+            //changing voice
+            synth.Volume = 70;  // 0...100
+            synth.SelectVoiceByHints(VoiceGender.Female);
+            synth.Speak("This is a good girl voice.");
+        }
+    }
+}
