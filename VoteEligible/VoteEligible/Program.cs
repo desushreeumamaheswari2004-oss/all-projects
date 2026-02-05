92d88964a5bce08b5b2750c941b1b3d1d374afe6
DESU SHREE UMA MAHESWARI <desushreeumamaheswari2004@gmail.com>
2026-02-05 15:03:50 +0530

    Add files via upload

---
+++ VoteEligible/VoteEligible/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter your age:");
+int age;
+bool isValidAge = int.TryParse(Console.ReadLine(), out age);
+if (isValidAge)
+{
+    if (age >= 18)
+    {
+        Console.WriteLine("Eligible");
+    }
+    else
+    {
+        Console.WriteLine("Not Eligible");
+    }
+}
+else
+{
+    Console.WriteLine("Input Error!!");
+}
